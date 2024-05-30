bdd1d2753a10bf77abcf8835322fc34a7d0c53aa
J Mackenzie <JMMackenzie@users.noreply.github.com>
2024-05-30 16:01:11 +1000

    Update download-data.sh
    Update data script since cloudstor has been decommissioned.

--- script/download-data.sh
+++ script/download-data.sh
@@ -1,24 +1,4 @@
 #!/usr/bin/env bash
 
-set -ex
-
-D=data
-
-# download
-wget https://cloudstor.aarnet.edu.au/plus/s/AYX0o3PB0fXtQ7p/download -O ioqp.zip
-unzip ioqp.zip -x ioqp/ccnews/*
-mv ioqp $D
-rm -v ioqp.zip
-
-# extract ciff
-for i in \
-    $D/gov2/ciff/bp-gov2.ciff.gz \
-    $D/msmarco/ciff/bp-deepct.ciff.gz \
-    $D/msmarco/ciff/bp-deepimpact.ciff.gz \
-    $D/msmarco/ciff/bp-doct5query.ciff.gz \
-    $D/msmarco/ciff/bp-original.ciff.gz \
-    $D/msmarco/ciff/bp-spladev2.ciff.gz \
-    $D/msmarco/ciff/bp-unicoil-tilde.ciff.gz
-do
-    gunzip -v $i
-done
+echo "You will require a unique URL with a password to get the data due to the old data storage platform being decommissioned."
+echo "Please create a github issue or contact Joel: joel.mackenzie@uq.edu.au"
